7ac155519f20adc1a74b50b728375d12d9c752ce
karlossg <karol.gratus@gmail.com>
2017-11-11 22:14:15 +0100

    starting popular dot js component

---
+++ app/components/App.js
@@ -0,0 +1,13 @@
+const React = require('react');
+
+class App extends React.Component {
+    render() {
+      return (
+        <div>
+          Hello there!
+        </div>
+      )
+    }
+  }
+
+module.exports = App;
