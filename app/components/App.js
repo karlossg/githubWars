7f55e5cda59311e2fbb6c4208eca3f25c990a015
Karol <karol.gratus@gmail.com>
2017-11-14 15:43:25 +0100

    battle + player input comoponent

--- app/components/App.js
+++ app/components/App.js
@@ -3,9 +3,11 @@ const Popular = require('./Popular');
 const ReactRouter = require('react-router-dom');
 const Router = ReactRouter.BrowserRouter;
 const Route = ReactRouter.Route;
+const Switch = ReactRouter.Switch;
 const Nav = require('./Nav');
 const Home = require('./Home');
 const Battle = require('./Battle')
+
 
 class App extends React.Component {
     render() {
@@ -13,9 +15,15 @@ class App extends React.Component {
         <Router>
           <div className='container'>
             <Nav />
-            <Route exact path='/' component={Home} />
-            <Route path='/battle' component={Battle} />
-            <Route path='/popular' component={Popular} />
+            <Switch>
+              <Route exact path='/' component={Home} />
+              <Route exact path='/battle' component={Battle} />
+              <Route path='/popular' component={Popular} />
+              <Route render={function () {
+                return <p>Not Found</p>
+              }} />
+            </Switch>
+            
           </div>
         </Router>
       )
--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,10 +1,52 @@
 const React = require('react');
 
+class PlayerInput extends React.Component {
+  render() {
+    return (
+      
+    )
+  }
+}
 class Battle extends React.Component {
-  render () {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      playerOneName: '',
+      playerTwoName: '',
+      playerOneImage: '',
+      playerTwoImage: ''
+    }
+  
+  this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  
+  handleSubmit(id, username) {
+    this.setState(function () {
+      let newState = {};
+      newState[id + 'Name'] = username;
+      newState[id + 'Image'] = 'https://github.com/' + username + '.png?size=200';
+      return newState;
+    })
+  }
+
+  render() {
+    let playerOneName = this.state.playerOneName;
+    let playerTwoName = this.state.playerTwoName;
     return (
       <div>
-        Battle!
+        <div className='row'>
+          {!playerOneName && 
+            <PlayerInput 
+              id='playerOne'
+              label='Player One'
+              onSubmit={this.handleSubmit}/>}
+          
+          {!playerTwoName && 
+            <PlayerInput id='playerTwo'
+            label='Player Two'
+            onSubmit={this.handleSubmit}/>}
+        </div>
       </div> 
     )
   }
