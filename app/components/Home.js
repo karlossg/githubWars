ad12ed3dc492875cb5db67d3ed53497cb2a235cf
Karol <karol.gratus@gmail.com>
2017-11-14 11:59:21 +0100

    gitignore

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,5 +1,12 @@
 const React = require('react');
 
 class Home extends React.Component {
-  
+  render() { <
+    div className = 'home-container' >
+      <
+      h1 > Github Battle: Fight with your github friends! < /h1> <
+      /div>
+  }
 }
+
+module.exports = Home;
