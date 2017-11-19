e8468198b47d26f0dd776b2fa19a130249b565d4
karlossg <karol.gratus@gmail.com>
2017-11-19 23:15:54 +0100

    playerpreview started

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,6 +1,26 @@
 const React = require('react');
 const PropTypes = require('prop-types');
 
+function PlayerPreview (props) {
+  return (
+    <div>
+      <div className='column'>
+        <img 
+          className='avatar' 
+          src={props.avatar}   
+          alt={'Avatar for ' + props.username} 
+        />
+      </div>
+    </div>
+  )
+}
+
+PlayerPreview.propTypes = {
+  avatar: PropTypes.string.isRequire,
+  username: PropTypes.string.isRequire,
+  onReset: PropTypes.func.isRequired
+}
+
 class PlayerInput extends React.Component {
   constructor(props) {
     super(props);
