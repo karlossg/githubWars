6de80e66429649334fe692000e997af1ee8aa318
Karol <karol.gratus@gmail.com>
2017-12-01 15:18:59 +0100

    get profile + get repos in api.js

--- app/utils/api.js
+++ app/utils/api.js
@@ -1,6 +1,25 @@
 const axios = require('axios');
 
+const id = "YOUR_CLIENT_ID";
+const sec = "YOUR_SECRET_ID";
+const params = "?client_id=" + id + "&client_secret=" + secret;
+
+function getProfile(username) {
+  return axios.get('https://api.github.com/users/' + username + params)
+    .then(function(user) {
+      return user.data;
+    });
+}
+
+function getRepos(username) {
+  return axios.get('https://api.github.com/users/' + username + '/repos' + params)
+}
+
+
 module.exports = {
+  battle: function (players) {
+    
+  },
   fetchPopularRepos: function (language) {
     const encodedURI = window.encodeURI('https://api.github.com/search/repositories?q=stars:>1+language:'+ language + '&sort=stars&order=desc&type=Repositories');
 
